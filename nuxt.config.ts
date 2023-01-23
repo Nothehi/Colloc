8f8a18877c72654c04e4816c4a8f1f07a86e9b4a
nothehi <nodehi7813@gmail.com>
2023-01-23 22:33:28 +0330

    Add head tags in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss'
-    ]
+    ],
+    app: {
+        head: {
+            title: 'ColloC',
+            meta: [
+                { name: 'author', content: 'Hossein Nodehi' }
+            ],
+            link: [
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap' },
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Anton&display=swap' },
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Patua+One&display=swap' },
+            ],
+        }
+    },
 })
