a1adcd5e76c713cc48b78a37e20ebfc3c00594d7
nothehi <nodehi7813@gmail.com>
2023-01-23 22:32:47 +0330

    Add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        comfortaa: ['Comfortaa', 'cursive'],
+        patua: ['Patua One', 'cursive'],
+        anton: ['Anton', 'sans-serif'],
+      },
+      boxShadow: {
+        'filled': '2px 3px 0 #000000',
+      },
+      dropShadow: {
+        'filled': '2px 3px 0 #000000',
+      }
+    },
+  },
+}
