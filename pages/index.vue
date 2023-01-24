21acf9c86ff35c585a2dc0d9ecaa684ac06e279e
nothehi <nodehi7813@gmail.com>
2023-01-24 22:33:14 +0330

    Add main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
+  <main class="flex flex-col items-center w-full">
   <div>
-    <NuxtWelcome />
+      <h1 class="font-bold text-3xl">ColloC</h1>
+      <h2 class="tracking-widest">collection's of collocation's</h2>
   </div>
+
+    <base-text-box placeholder="Search &quot;Greetings&quot;">
+      <template #prefix>
+        <search-icon />
+      </template>
+    </base-text-box>
+  </main>
 </template>
 
 <script lang="ts" setup></script>
