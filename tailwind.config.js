e7d796e11c13199a8f55a7fdc12d96474fe15111
nothehi <nodehi7813@gmail.com>
2023-09-30 15:45:41 +0330

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        comfortaa: ['Comfortaa', 'cursive'],
-        patua: ['Patua One', 'cursive'],
-        anton: ['Anton', 'sans-serif'],
-      },
-      boxShadow: {
-        'neuburtal-sm': '.125rem .125rem 0rem #000000',
-        'neuburtal': '.25rem .25rem 0rem #000000',
-        'neuburtal-md': '.375rem .375rem 0rem #000000',
-        'neuburtal-lg': '.5rem .5rem 0rem #000000',
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				comfortaa: ['Comfortaa', 'cursive'],
+				patua: ['Patua One', 'cursive'],
+				mplus: ['\'M PLUS Rounded 1c\'', 'sans-serif'],
+				vazir: ['Vazirmatn', 'sans-serif'],
+			},
+			boxShadow: {
+				'neuburtal-sm': '.125rem .125rem 0rem #000000',
+				'neuburtal': '.25rem .25rem 0rem #000000',
+				'neuburtal-md': '.375rem .375rem 0rem #000000',
+				'neuburtal-lg': '.5rem .5rem 0rem #000000',
+			},
+		},
+	},
+	plugins: [],
 }
