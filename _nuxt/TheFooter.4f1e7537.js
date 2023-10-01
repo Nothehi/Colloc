a014aabb09df9528e1850701690097cb988b513e
nothehi <nodehi7813@gmail.com>
2023-10-01 12:20:52 +0330

    Add new build

--- _nuxt/TheFooter.4f1e7537.js
+++
@@ -1,1 +0,0 @@
-import{J as t,o,E as s,F as e,N as c}from"./entry.bcc17af8.js";const _={},a={class:"absolute bottom-0 w-full p-4"},n=e("p",null,[c("Made with \u2764\uFE0F by "),e("a",{href:"https://github.com/Nothehi"},"Nothehi")],-1),r=[n];function h(i,l){return o(),s("footer",a,r)}const f=t(_,[["render",h]]);export{f as default};
---
+++ assets/Collocation-573f2680.js
@@ -0,0 +1,1 @@
+import{r as n,o as s,c as _}from"./index-6fd8d6da.js";const a=(e,t)=>{const o=e.__vccOpts||e;for(const[r,c]of t)o[r]=c;return o},f={};function l(e,t){const o=n("router-view");return s(),_(o)}const i=a(f,[["render",l]]);export{i as default};
