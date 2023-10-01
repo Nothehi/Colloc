df9d35f21ad017abb1eca79a55ee0635d983bfff
nothehi <nodehi7813@gmail.com>
2023-10-01 11:15:40 +0330

    Add home view

---
+++ src/components/TheCollection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { type Item, useCollocation } from '@/stores/collocation'
+
+interface Props {
+    item: Item,
+    category: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <router-link :to="{ name: 'collection', params: { category, collection: useCollocation().getSlug(item.title) } }">
+        <img 
+            :src="item.image"
+            class="shadow-neuburtal w-40 h-60 rounded-lg border-black border-2 hover:shadow-neuburtal-sm transition-all 
+                hover:translate-x-0.5 hover:translate-y-0.5 active:saturate-50">
+    </router-link>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useCollocation } from '@/stores/collocation'
+import TheCollection from '@/components/TheCollection.vue'
+
+const collocation = useCollocation()
+</script>
+
+<template>
+    <main class="flex flex-col items-center justify-center w-full h-screen font-comfortaa">
+        <div class="text-center">
+            <h1 class="text-5xl font-bold">ColloC</h1>
+            <h2 class="tracking-widest">collection's of collocation's</h2>
+        </div>
+
+        <ul class="p-5 mt-10">
+            <li
+                v-for="(book, idx) in collocation.collection.books" 
+                :key="idx"
+                class="inline-block w-40 p-2">
+                <the-collection :item="book" category="books" />
+            </li>
+        </ul>
+
+        <footer class="absolute bottom-0 w-full p-4 text-center">
+            <p>Made with ❤️ by <a href="https://github.com/Nothehi">Nothehi</a></p>
+        </footer>
+    </main>
+</template>
